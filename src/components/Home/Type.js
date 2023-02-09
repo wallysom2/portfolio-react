5d5f4a88b3236c4c6ff3994e701ed3a19ddacbab
matheus <wallysom2@gmail.com>
2023-02-09 11:39:59 -0300

    first commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Developer",
+          "MERN Stack Developer",
+          "Full-Stack Web Developer",
+          "Software Engineer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
